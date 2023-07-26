d5116e6c39214959ef0dfe43f7b1180829439327
Cai <1357618897@qq.com>
2023-07-26 17:10:54 +0800

    feat(components): ✨ 完成投诉反馈和关于我们组件的搭建和完成

--- src/views/about/index.vue
+++ src/views/about/index.vue
@@ -1,7 +1,43 @@
 <template>
-  <div>关于我们</div>
+  <div class="container">
+    <h2>关于我们</h2>
+    <p>
+      同意建设西安市道路客运联网售票系统。该系统由西安市交通运输厅投资建设，是惠及群众、服务社会的民生工程。乘客通过“西安汽车客运票务网”购买汽车票，免收服务费
+    </p>
+    <p>
+      西安汽车客运票务网是西安道路客运服务广大社会公众的重要窗口。由西安市交通运输厅道路运输管理局全力打造，通过集成全省道路客运信息，为广大社会公众免费提供在线汽车余票、时刻表、汽车站信息查询，网上购票以及在线咨询等综合性服务。
+    </p>
+    <p>
+      我们承诺，将通过多项专利技术的转化和技术创新，努力改善旅客的购票体验，恪尽职守，逐步充实服务信息，扩大服务范围，完善界面效果，为广大社会公众提供更方便、快捷、丰富、优质的道路客运票务服务。
+    </p>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script lang="ts" setup></script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.container {
+  padding: 30px 80px;
+  width: 100%;
+  min-height: 600px;
+  border: 1px solid #5678a8;
+  h2 {
+    font-size: 20px;
+    color: #e27500;
+    padding-bottom: 45px;
+    font-weight: bold;
+  }
+  p {
+    color: #00618b;
+    font-weight: 600;
+    padding-bottom: 45px;
+    font-size: 18px;
+    line-height: 1.5;
+    text-indent: 2em;
+  }
+  h5 {
+    color: #e27500;
+    font-size: 17px;
+  }
+}
+</style>
--- src/views/advise/index.vue
+++ src/views/advise/index.vue
@@ -1,7 +1,43 @@
 <template>
-  <div>投诉建议</div>
+  <div class="container">
+    <h2>投诉建议渠道</h2>
+    <p>
+      我们承诺，将通过多项专利技术的转化和技术创新，努力改善旅客的购票体验，恪尽职守，逐步充实服务信息，扩大服务范围，完善界面效果，为广大社会公众提供更方便、快捷、丰富、优质的道路客运票务服务。
+    </p>
+    <p>
+      我们欢迎每一位用户对我们的服务进行监督，也接受您提供的建议，如果由于我们工作的疏忽造成了某些失误，给您带来了不便，或者您对我们的服务有更好的建议,您可以通过电话联系我们。
+    </p>
+    <h5>客服电话：400-858-0000</h5>
+    <h5>工作时间：(08:00-20:00)</h5>
+    <p style="padding-top: 45px">客服专员将第一时间为您处理，非常感谢对陕西省客运互联网售票平台的督促和支持!</p>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script lang="ts" setup></script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.container {
+  padding: 30px 80px;
+  width: 100%;
+  min-height: 600px;
+  border: 1px solid #5678a8;
+  h2 {
+    font-size: 20px;
+    color: #e27500;
+    padding-bottom: 45px;
+    font-weight: bold;
+  }
+  p {
+    color: #00618b;
+    font-weight: 600;
+    padding-bottom: 45px;
+    font-size: 18px;
+    line-height: 1.5;
+    text-indent: 2em;
+  }
+  h5 {
+    color: #e27500;
+    font-size: 17px;
+  }
+}
+</style>
