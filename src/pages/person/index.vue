30d426ad2a6f116515c81a1af0e33595e19fb5c0
Cai <1357618897@qq.com>
2023-08-01 20:04:41 +0800

    fix(element-ui): 🐛 修改路由导航刷新无法动态选中选项和无法跳转组件问题

--- src/pages/person/index.vue
+++ src/pages/person/index.vue
@@ -4,38 +4,38 @@
       <el-col :span="6">
         <!-- 左侧导航栏 -->
         <div class="leftMenu">
-          <el-menu default-active="1" class="el-menu-vertical-demo">
+          <el-menu :default-active="$route.path" class="el-menu-vertical-demo">
             <h3>账户管理</h3>
-            <el-menu-item index="1">
+            <el-menu-item index="/person/baseInfo" @click="changeActive('/person/baseInfo')">
               <el-icon><User /></el-icon>
               <span>基本资料</span>
             </el-menu-item>
-            <el-menu-item index="2">
+            <el-menu-item index="/person/changePassword" @click="changeActive('/person/changePassword')">
               <el-icon><Unlock /></el-icon>
               <span>修改密码</span>
             </el-menu-item>
-            <el-menu-item index="3">
+            <el-menu-item index="/person/passenger" @click="changeActive('/person/passenger')">
               <el-icon><Van /></el-icon>
               <span>常用乘车人</span>
             </el-menu-item>
-            <el-menu-item index="4">
+            <el-menu-item index="/person/contacts" @click="changeActive('/person/contacts')">
               <el-icon><Message /></el-icon>
               <span>常用联系人</span>
             </el-menu-item>
             <h3>订单管理</h3>
-            <el-menu-item index="5">
+            <el-menu-item index="/person/allOrder" @click="changeActive('/person/allOrder')">
               <el-icon><List /></el-icon>
               <span>所有订单</span>
             </el-menu-item>
-            <el-menu-item index="6">
+            <el-menu-item index="/person/paidOrder" @click="changeActive('/person/paidOrder')">
               <el-icon><Checked /></el-icon>
               <span>已支付订单</span>
             </el-menu-item>
-            <el-menu-item index="7">
+            <el-menu-item index="/person/waitOrder" @click="changeActive('/person/waitOrder')">
               <el-icon><Clock /></el-icon>
               <span>待支付订单</span>
             </el-menu-item>
-            <el-menu-item index="8">
+            <el-menu-item index="/person/refundOrder" @click="changeActive('/person/refundOrder')">
               <el-icon><Wallet /></el-icon>
               <span>退款</span>
             </el-menu-item>
@@ -52,6 +52,16 @@
 <script lang="ts" setup>
 //引入图标
 import { User, Unlock, List, Checked, Clock, Wallet, Message, Van } from '@element-plus/icons-vue'
+//引入路由器跳转
+import { useRouter, useRoute } from 'vue-router'
+//获取当前路由信息来修改刷新高亮不成功问题
+let $route = useRoute()
+//创建路由器实例
+const $router = useRouter()
+//左侧路由切换点击的回调函数
+const changeActive = (path: string) => {
+  $router.push({ path })
+}
 </script>
 
 <style lang="scss" scoped>
