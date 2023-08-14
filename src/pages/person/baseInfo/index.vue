60a48b60b6c5c23973ee567e9751aa6f79bb746d
Cai <1357618897@qq.com>
2023-08-14 17:24:11 +0800

    feat(request): ✨ 完成对个人信息修改的功能，修改相应字段，同时增加校验规则

--- src/pages/person/baseInfo/index.vue
+++ src/pages/person/baseInfo/index.vue
@@ -10,21 +10,21 @@
       <div class="userInfo">
         <div class="userInfo-title">基础设置</div>
         <!-- 展示基本的个人信息 -->
-        <el-form ref="form" :model="userForm" label-width="100px">
+        <el-form ref="form" :model="userStore.userInfo" label-width="100px">
           <el-form-item label="用户名：">
-            <el-input v-model="userForm.nickname" disabled class="formName"></el-input>
+            <el-input v-model="userStore.userInfo.nickname" disabled class="formName"></el-input>
           </el-form-item>
           <el-form-item label="真实姓名：">
-            <el-input v-model="userForm.name" disabled class="formName"></el-input>
+            <el-input v-model="userStore.userInfo.name" disabled class="formName"></el-input>
           </el-form-item>
           <el-form-item label="性&nbsp;&nbsp;&nbsp;别：">
-            <el-input v-model="userForm.sex" disabled class="formName"></el-input>
+            <el-input v-model="userStore.userInfo.sex" disabled class="formName"></el-input>
           </el-form-item>
           <el-form-item label="证件类型：">
             <el-input v-model="cardType" disabled style="width: 300px"></el-input>
           </el-form-item>
           <el-form-item label="证件号：">
-            <el-input v-model="userForm.card_number" style="width: 300px" disabled></el-input>
+            <el-input v-model="userStore.userInfo.cardNumber" style="width: 300px" disabled></el-input>
           </el-form-item>
         </el-form>
       </div>
@@ -42,9 +42,9 @@
           <div class="userInfo-title">手机绑定</div>
           <div class="userInfo-body">
             <!-- 展示基本的个人信息 -->
-            <el-form ref="form" :model="userForm" label-width="100px">
+            <el-form ref="form" :model="userStore.userInfo" label-width="100px">
               <el-form-item label="手机：">
-                <el-input v-model="userForm.phone_number" disabled style="width: 300px"></el-input>
+                <el-input v-model="userStore.userInfo.phoneNumber" disabled style="width: 300px"></el-input>
               </el-form-item>
             </el-form>
           </div>
@@ -55,9 +55,9 @@
 
           <div class="userInfo-body">
             <!-- 展示基本的个人信息 -->
-            <el-form ref="form" :model="userForm" label-width="100px">
+            <el-form ref="form" :model="userStore.userInfo" label-width="100px">
               <el-form-item label="邮箱：">
-                <el-input v-model="userForm.email" disabled style="width: 300px"></el-input>
+                <el-input v-model="userStore.userInfo.email" disabled style="width: 300px"></el-input>
               </el-form-item>
             </el-form>
           </div>
@@ -67,8 +67,8 @@
     <!-- 修改基本信息的对话框 -->
     <el-dialog v-model="dialogVisible" title="修改信息" width="30%">
       <!-- 修改用户信息表单数据 -->
-      <el-form ref="form" :model="editForm" label-width="100px">
-        <el-form-item label="用户名：">
+      <el-form :model="editForm" label-width="100px" :rules="rules" ref="ruleFormRef">
+        <el-form-item label="用户名：" prop="nickname">
           <el-input v-model="editForm.nickname" style="width: 200px"></el-input>
         </el-form-item>
         <el-form-item label="真实姓名：">
@@ -80,12 +80,12 @@
           </el-select>
         </el-form-item>
         <el-form-item label="证件类型：">
-          <el-select v-model="editForm.card_type" class="m-2" size="large" placeholder="请选择身份证类型">
+          <el-select v-model="editForm.cardType" class="m-2" size="large" placeholder="请选择身份证类型">
             <el-option v-for="item in cardOpt" :key="item.value" :label="item.label" :value="item.value" />
           </el-select>
         </el-form-item>
-        <el-form-item label="证件号：">
-          <el-input v-model="editForm.card_number" style="width: 300px"></el-input>
+        <el-form-item label="证件号：" prop="cardNumber">
+          <el-input v-model="editForm.cardNumber" style="width: 300px"></el-input>
         </el-form-item>
       </el-form>
       <template #footer>
@@ -98,18 +98,18 @@
     <!-- 手机邮箱的绑定 -->
     <el-dialog v-model="bindDialogVisible" title="修改信息" width="30%">
       <!-- 修改用户信息表单数据 -->
-      <el-form ref="form" :model="editForm" label-width="100px">
-        <el-form-item label="邮箱绑定：">
+      <el-form :model="editForm" label-width="100px" :rules="rules" ref="ruleFormRef">
+        <el-form-item label="邮箱绑定：" prop="email">
           <el-input v-model="editForm.email" style="width: 200px"></el-input>
         </el-form-item>
-        <el-form-item label="手机绑定：">
-          <el-input v-model="editForm.phone_number" style="width: 200px"></el-input>
+        <el-form-item label="手机绑定：" prop="phoneNumber">
+          <el-input v-model="editForm.phoneNumber" style="width: 200px"></el-input>
         </el-form-item>
       </el-form>
       <template #footer>
         <span class="dialog-footer">
           <el-button @click="bindDialogVisible = false" size="large">取消</el-button>
-          <el-button type="primary" @click="bindDialogVisible = false" size="large">确定</el-button>
+          <el-button type="primary" @click="editInfo()" size="large">确定</el-button>
         </span>
       </template>
     </el-dialog>
@@ -119,6 +119,7 @@
 <script lang="ts" setup>
 import { User, EditPen, Paperclip } from '@element-plus/icons-vue'
 import { reactive, ref, computed, onMounted } from 'vue'
+
 //引入个人仓库
 import useUserStore from '@/store/user'
 //控制证件类型的选择器
@@ -169,64 +170,73 @@ let userStore = useUserStore()
 let dialogVisible = ref<boolean>(false)
 //控制绑定信息的对话框显示与隐藏
 let bindDialogVisible = ref<boolean>(false)
-
+//定义校验规则
+const rules = reactive({
+  nickname: [{ required: 'true', message: '请输入昵称', trigger: 'blur' }],
+  cardNumber: [{ required: 'true', message: '请输入您的证件号', trigger: 'blur' }],
+  email: [
+    { required: 'true', message: '请输入邮箱', trigger: 'blur' },
+    { pattern: /^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/, message: '邮箱格式不正确', trigger: 'blur' }
+  ],
+  phoneNumber: [
+    { required: 'true', message: '请输入手机号', trigger: 'blur' },
+    { pattern: /^1[3456789]\d{9}$/, message: '手机号格式不正确', trigger: 'blur' }
+  ]
+})
+//定义校验规则
+const ruleFormRef = ref(null)
 //计算属性计算证件类型
 const cardType = computed(() => {
-  if (userStore.userInfo.card_type === '1') {
+  if (userStore.userInfo.cardType === '1') {
     return '身份证'
   }
-  if (userStore.userInfo.card_type === '2') {
+  if (userStore.userInfo.cardType === '2') {
     return '军人证'
   }
-  if (userStore.userInfo.card_type === '3') {
+  if (userStore.userInfo.cardType === '3') {
     return '护照'
   }
-  if (userStore.userInfo.card_type === '4') {
+  if (userStore.userInfo.cardType === '4') {
     return '港澳居民来往内地通行证'
   }
-  if (userStore.userInfo.card_type === '5') {
+  if (userStore.userInfo.cardType === '5') {
     return '台湾居民来往内地通行证'
   }
-  if (userStore.userInfo.card_type === '6') {
+  if (userStore.userInfo.cardType === '6') {
     return '港澳台居民居住证'
   }
 })
-//用户信息
-const userForm = ref({
-  nickname: '',
-  name: '',
-  sex: '',
-  card_type: '',
-  card_number: '',
-  phone_number: '',
-  email: ''
-})
 //修改用户信息
-const editForm = ref({
+const editForm = ref<any>({
   nickname: '',
   name: '',
   sex: '',
-  card_type: '',
-  card_number: '',
-  phone_number: '',
+  cardType: '',
+  cardNumber: '',
+  phoneNumber: '',
   email: ''
 })
 
 onMounted(() => {
-  userForm.value = userStore.userInfo
   //修改用户信息
   //深拷贝给修改用户的表单
   //避免修改时候 原用户信息改变
-  editForm.value = JSON.parse(JSON.stringify(userForm.value))
+  editForm.value = JSON.parse(JSON.stringify(userStore.userInfo))
   // console.log({ ...editForm.value })
 })
 
 //调用修改用户信息的接口
 const editInfo = () => {
-  // console.log({ ...editForm.value })
-  //调用修改信息的接口
-  userStore.editUserInfo({ ...editForm.value })
-  dialogVisible.value = false
+  let { validate } = ruleFormRef.value as any
+  validate((valid: Boolean) => {
+    //通过校验才发起请求
+    if (valid) {
+      //调用修改信息的接口
+      userStore.editUserInfo({ ...editForm.value })
+      dialogVisible.value = false
+      bindDialogVisible.value = false
+    }
+  })
 }
 </script>
 
--- src/store/user.ts
+++ src/store/user.ts
@@ -1,5 +1,6 @@
 //定义用户相关的仓库
 import { defineStore } from 'pinia'
+import { ElMessage } from 'element-plus'
 //引入接口
 import { reqUserLogin, reqUserInfo, reqUserInfoEdit } from '@/api/user/index'
 //引入类型
@@ -9,7 +10,16 @@ const useUserStore = defineStore('User', {
   state: () => {
     return {
       //用户信息,
-      userInfo: JSON.parse(localStorage.getItem('userInfo') as string) || {},
+      userInfo: JSON.parse(localStorage.getItem('userInfo') as string) || {
+        cardNumber: '',
+        cardType: '',
+        email: '',
+        name: '',
+        nickname: '',
+        phoneNumber: '',
+        photo: '',
+        sex: ''
+      },
       //用户登录token
       userToken: JSON.parse(localStorage.getItem('token') as string)
     }
@@ -19,9 +29,29 @@ const useUserStore = defineStore('User', {
     async getUserInfo() {
       let result: userProfileInfoType = await reqUserInfo()
       if (result.code === 200) {
-        console.log(result)
         //本地持久化存储
-        this.userInfo = result.profileInfo
+
+        const {
+          card_number: cardNumber,
+          card_type: cardType,
+          email,
+          name,
+          nickname,
+          phone_number: phoneNumber,
+          photo,
+          sex
+        } = result.profileInfo
+        //解构赋值 重新定义字段存储仓库
+        this.userInfo = {
+          cardNumber,
+          cardType,
+          email,
+          name,
+          nickname,
+          phoneNumber,
+          photo,
+          sex
+        }
         localStorage.setItem('userInfo', JSON.stringify(this.userInfo))
       }
     },
@@ -43,9 +73,17 @@ const useUserStore = defineStore('User', {
       // 仓库重新存储
       if (result.code === 200) {
         this.userInfo = params
-        localStorage.setItem('userInfo', JSON.stringify(params))
+        await this.getUserInfo()
+        ElMessage({
+          type: 'success',
+          message: '修改成功'
+        })
+      } else {
+        ElMessage({
+          type: 'error',
+          message: '修改失败'
+        })
       }
-      console.log(result)
     },
     //退出登录函数
     //清除本地存储的个人信息和token
